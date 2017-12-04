008e5b04dab4bc064314fd3434218a28fc12ba0e
gh-Devin <18310451502@163.com>
2017-12-04 17:30:45 +0800

    update name

--- simple_project/lol_game.py
+++ simple_project/lol_game.py
@@ -14,13 +14,13 @@ secon_user_password  = raw_input('please input your User_Password : ')
 
 # 验证两次密码准确度
 if user_password == secon_user_password:
-	print "恭喜注册成功！"
+    print "恭喜注册成功！"
 
 elif user_password != secon_user_password:
-	print "您输入的密码不一致，请重新输入"
-	user_password  = raw_input('please input your User_Password : ')
-	secon_user_password  = raw_input('please input your User_Password : ')
-	print "恭喜注册成功！"
+    print "您输入的密码不一致，请重新输入"
+    user_password  = raw_input('please input your User_Password : ')
+    secon_user_password  = raw_input('please input your User_Password : ')
+    print "恭喜注册成功！"
 
 print "欢迎进入英雄联盟用户界面"
 # 获取用户输入的游戏名
